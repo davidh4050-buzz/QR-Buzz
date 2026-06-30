f610a3526bf1c59f149194eb8d2b4fb87390abdc
davidh4050-buzz <davidh4050@gmail.com>
2026-06-30 18:25:57 +0100

    Create Plugin.php

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace QRBuzz;
+
+use QRBuzz\Admin\AdminPage;
+
+class Plugin
+{
+    public function init(): void
+    {
+        if (is_admin()) {
+            (new AdminPage())->init();
+        }
+    }
+}
