49b35b81df98dc436c054481cedee40fd1045aab
davidh4050-buzz <davidh4050@gmail.com>
2026-07-01 11:03:38 +0100

    Add files via upload

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace QRBuzz;
 
 use QRBuzz\Admin\AdminPage;
 
-class Plugin
-{
-    public function init(): void
-    {
+class Plugin {
+    public function init(): void {
         if (is_admin()) {
             (new AdminPage())->init();
         }
