2fb069088f1ef74558dd15a5696004254ba52679
davidh4050-buzz <davidh4050@gmail.com>
2026-07-01 13:23:08 +0100

    Register QR Buzz services

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,11 +2,31 @@
 namespace QRBuzz;
 
 use QRBuzz\Admin\AdminPage;
+use QRBuzz\Database\Installer;
+use QRBuzz\Database\QRRepository;
+use QRBuzz\Redirect\RedirectHandler;
 
 class Plugin {
+
+    private QRRepository $repository;
+
+    public function __construct(?QRRepository $repository = null) {
+        $this->repository = $repository ?: new QRRepository();
+    }
+
     public function init(): void {
+        $this->maybeUpgradeDatabase();
+        (new RedirectHandler($this->repository))->init();
+
         if (is_admin()) {
-            (new AdminPage())->init();
+            (new AdminPage($this->repository))->init();
+        }
+    }
+
+    private function maybeUpgradeDatabase(): void {
+        if (get_option('qrbuzz_db_version') !== QR_BUZZ_VERSION) {
+            Installer::createTables();
+            update_option('qrbuzz_db_version', QR_BUZZ_VERSION);
         }
     }
 }
