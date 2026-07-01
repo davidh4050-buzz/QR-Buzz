57b3ee7d28ba79e56151bee6324d173d30e9d7dd
davidh4050-buzz <davidh4050@gmail.com>
2026-07-01 15:22:35 +0100

    Add PNG and SVG QR output methods

--- src/QR/QRGenerator.php
+++ src/QR/QRGenerator.php
@@ -5,10 +5,39 @@ use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\SvgWriter;
 
 class QRGenerator {
 
     public function generate(string $data, int $size = 300): string {
+        return $this->generatePngDataUri($data, $size);
+    }
+
+    public function generatePngDataUri(string $data, int $size = 300): string {
+        return (new PngWriter())->write($this->qrCode($data, $size))->getDataUri();
+    }
+
+    public function generatePng(string $data, int $size = 600): string {
+        return (new PngWriter())->write($this->qrCode($data, $size))->getString();
+    }
+
+    public function generateSvg(string $data, int $size = 600): string {
+        if (!class_exists(SvgWriter::class)) {
+            throw new \RuntimeException('SVG QR downloads are not available with the bundled QR library.');
+        }
+
+        return (new SvgWriter())->write($this->qrCode($data, $size))->getString();
+    }
+
+    public function supportsSvg(): bool {
+        return class_exists(SvgWriter::class);
+    }
+
+    public function trackingUrl(string $shortcode): string {
+        return home_url('/q/' . rawurlencode($shortcode));
+    }
+
+    private function qrCode(string $data, int $size): QrCode {
         $data = trim($data);
 
         if ($data === '') {
@@ -25,12 +54,6 @@ class QRGenerator {
         $qrCode->setSize($size);
         $qrCode->setMargin(12);
 
-        $result = (new PngWriter())->write($qrCode);
-
-        return $result->getDataUri();
-    }
-
-    public function trackingUrl(string $shortcode): string {
-        return home_url('/q/' . rawurlencode($shortcode));
+        return $qrCode;
     }
 }
