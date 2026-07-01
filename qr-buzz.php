f2dbb44ed622c07918969766ea495f0138208d57
davidh4050-buzz <davidh4050@gmail.com>
2026-07-01 12:55:19 +0100

    Bump QR Buzz to v0.2.0

--- qr-buzz.php
+++ qr-buzz.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: QR Buzz
- * Description: QR code generation for WordPress (v0.1.0)
- * Version: 0.1.0
+ * Description: Local QR code generation for WordPress (v0.2.0)
+ * Version: 0.2.0
  * Author: QR Buzz
  */
 
 if (!defined('ABSPATH')) exit;
 
-define('QR_BUZZ_VERSION', '0.1.0');
+define('QR_BUZZ_VERSION', '0.2.0');
 define('QR_BUZZ_PATH', plugin_dir_path(__FILE__));
 define('QR_BUZZ_URL', plugin_dir_url(__FILE__));
 
